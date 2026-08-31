54cfb9058a71c12585ed8cd84c0541e4267b0109
Nada Mamdouh <mohamedmamdouhnada@gmail.com>
2026-08-31 16:47:48 +0300

    Remove exposed API key

--- lib/Core/App_Apis/app_apis.dart
+++ lib/Core/App_Apis/app_apis.dart
@@ -6,7 +6,7 @@ class AppApis {
 
   // API Key
   static const String apiKey = '2dfe23358236069710a379edd4c65a6b';
-  static const String geminiApiKey = 'AQ.Ab8RN6Jszfumyq3rWSBYurJ62xduzudUO1oDc-gpJRUfQK7YJQ';
+  static const String geminiApiKey = '';
 
   // Endpoints
   static const String popularPersons = '$baseUrl/person/popular?api_key=$apiKey';
